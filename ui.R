4406268d38e05bc85f8bdea770cf9eda808bad7c
Yong Jiang <yojiang@syr.edu>
2015-12-12 15:12:14 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,8 +10,9 @@ fluidPage(
   sidebarPanel(
     
     #create variables "year“，we can add more here
-    selectInput('year', 'Year', c("1","2","3","4","5","6","7","8","9"))
-    
+    selectInput('year', 'Year', c("1","2","3","4","5","6","7","8","9")),
+    selectInput('Documentation', 'Documentation', c("I-551 CU","I-551 F","I-688B","I-766 a10","I-94","I-94 order of supervision","Prima Facie"))
+    ),
   ),
   
   #mainpanel
